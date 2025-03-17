160020be095a5b3a695dc876268bef0f851e1c1f
Your Name <you@example.com>
2025-03-16 22:37:53 -0300

    feat: Criada a classe MonitoramentoLogController com endpoint para salvar logs

--- api/src/main/java/com/monitoramento/api/service/MonitoramentoLogService.java
+++ api/src/main/java/com/monitoramento/api/service/MonitoramentoLogService.java
@@ -4,17 +4,18 @@ package com.monitoramento.api.service;
 import com.monitoramento.api.model.MonitoramentoLog;
 import com.monitoramento.api.repository.MonitoramentoLogRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MonitoramentoLogService {
 
+    @Autowired
+    private MonitoramentoLogRepository monitoramentoLogRepository;
 
-    private final  MonitoramentoLogRepository monitoramentoLogRepository;
 
-
-    public MonitoramentoLog salvar(MonitoramentoLog log){
+    public MonitoramentoLog salvarLog(MonitoramentoLog log){
         return monitoramentoLogRepository.save(log);
     }
 
