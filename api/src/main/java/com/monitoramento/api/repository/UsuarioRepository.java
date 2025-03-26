bccc8bf3f4dda46a4a6213822865e99972f80e4f
Your Name <you@example.com>
2025-03-25 22:26:51 -0300

    feat: Implementando a logica de autenticação :
    - Criação da classe 'Usuario' com os atributos: id (Long), email (String), senha (String) e role (Role).
    - Adição da enum 'Role' para definir os papéis de usuário: ADMIN e USER.
    - Implementação do repositório 'UsuarioRepository' com o método 'findByEmail' para busca de usuário por e-mail
    - Definição de 'email' como único e não nulo no banco de dados, garantindo integridade e consistência das informações.
    
    Essas alterações formam a base para a autenticação e gerenciamento de usuários no sistema.

---
+++ api/src/main/java/com/monitoramento/api/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.monitoramento.api.repository;
+
+
+import com.monitoramento.api.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+
+}
